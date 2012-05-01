dc96782baa2e28dc9f50243e2e2c534377f2dd91
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-05-01 15:21:20 +0200

    refactored tests

--- spec/simple-spec.js
+++ spec/simple-spec.js
@@ -13,6 +13,93 @@ describe("Simple", function () {
         expect(Simple.Model).toBeDefined();
     });
 
+    describe("Creation of new children", function() {
+        it("enables creation of new children by using extend", function() {
+            var Model = Simple.Model.extend({
+                test: function() {
+                    return "test";
+                }
+            });
+
+            var model = new Model();
+
+            expect(model.test).toBeDefined();
+        });
+
+        it("enables further child creation from a created child", function() {
+            var Model = Simple.Model.extend({
+                test: function() {
+                    return "child1";
+                }
+            });
+
+            var NewModel = Model.extend({
+                test2: function() {
+                    return "child2";
+                }
+            });
+
+            var model = new NewModel();
+
+            expect(model.test()).toMatch("child1");
+            expect(model.test2()).toMatch("child2");
+        });
+
+        it("can call properties on parent", function() {
+            Simple.Model.prototype.test = function() {
+                return "test";
+            };
+
+            var Model = Simple.Model.extend({});
+
+            var model = new Model();
+
+            expect(model.test()).toMatch("test");
+        });
+
+        it("favors properties in child over properties in parent", function() {
+            Simple.Model.prototype.test = function() {
+                return "parent";
+            };
+
+            var Model = Simple.Model.extend({
+                test: function() {
+                    return "child";
+                }
+            });
+
+            var model = new Model();
+
+            expect(model.test()).toMatch("child");
+
+            delete Simple.Model.prototype.test;
+        });
+
+        it("reflects the parent's prototype changes after child is created", function() {
+            var Model = Simple.Model.extend({});
+            var model = new Model();
+
+            Simple.Model.prototype.test = function() {
+                return "parent";
+            };
+
+            expect(model.test()).toMatch("parent");
+        });
+
+        it("calls initialize with all parameters when model in initialized", function() {
+            var spy = this.spy();
+
+            var Model = Simple.Model.extend({
+                initialize: spy
+            });
+
+            new Model("test", "test2");
+
+            expect(spy).toHaveBeenCalledOnceWith("test", "test2");
+        });
+
+    });
+
     describe("View", function() {
         it("enables initialization of views using new", function() {
             var view = new Simple.View();
@@ -20,58 +107,8 @@ describe("Simple", function () {
             expect(view instanceof Simple.View).toBeTrue();
         });
 
-        it("enables creation of new view classes through extend", function() {
-            var View = Simple.View.extend({
-                test: function() {
-                    return "test";
-                }
-            });
-
-            var view = new View();
-
-            expect(view.test).toBeDefined();
-        });
-
-        it("can call properties on parent", function() {
-            Simple.View.prototype.test = function() {
-                return "test";
-            };
-
-            var View = Simple.View.extend({});
-
-            var view = new View();
-
-            expect(view.test()).toMatch("test");
-        });
-
-        it("favors properties in child over properties in parent", function() {
-            Simple.View.prototype.test = function() {
-                return "parent";
-            };
-
-            var View = Simple.View.extend({
-                test: function() {
-                    return "child";
-                }
-            });
-
-            var view = new View();
-
-            expect(view.test()).toMatch("child");
-
-            delete Simple.View.prototype.test;
-        });
-
-        it("calls initialize with all parameters when view in initialized", function() {
-            var spy = this.spy();
-
-            var View = Simple.View.extend({
-                initialize: spy
-            });
-
-            new View("test", "test2");
-
-            expect(spy).toHaveBeenCalledOnceWith("test", "test2");
+        it("enables creation of new views", function() {
+            expect(Simple.View.extend).toBeDefined();
         });
 
         describe("render template", function() {
@@ -116,59 +153,8 @@ describe("Simple", function () {
             expect(model instanceof Simple.Model).toBeTrue();
         });
 
-        it("enables creation of new model classes through extend", function() {
-            var Model = Simple.Model.extend({
-                test: function() {
-                    return "test";
-                }
-            });
-
-            var model = new Model();
-
-            expect(model.test).toBeDefined();
+        it("enables creation of new models", function() {
+            expect(Simple.Model.extend).toBeDefined();
         });
-
-        it("can call properties on parent", function() {
-            Simple.Model.prototype.test = function() {
-                return "test";
-            };
-
-            var Model = Simple.Model.extend({});
-
-            var model = new Model();
-
-            expect(model.test()).toMatch("test");
-        });
-
-        it("favors properties in child over properties in parent", function() {
-            Simple.Model.prototype.test = function() {
-                return "parent";
-            };
-
-            var Model = Simple.Model.extend({
-                test: function() {
-                    return "child";
-                }
-            });
-
-            var model = new Model();
-
-            expect(model.test()).toMatch("child");
-
-            delete Simple.Model.prototype.test;
-        });
-
-        it("calls initialize with all parameters when model in initialized", function() {
-            var spy = this.spy();
-
-            var Model = Simple.Model.extend({
-                initialize: spy
-            });
-
-            new Model("test", "test2");
-
-            expect(spy).toHaveBeenCalledOnceWith("test", "test2");
-        });
-
     });
 });
