4a0b136b8f1b4f492bea1af69d98684bab9d94d5
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-10-24 22:16:42 +0200

    killing a global variable

--- simple.js
+++ simple.js
@@ -101,7 +101,7 @@
           var args = Array.prototype.slice.call(arguments, 1);
           var callbacks = this._callbacks || {};
           var events = callbacks[event] || [];
-          for (i = 0; i < events.length; i++) {
+          for (var i = 0; i < events.length; i++) {
             events[i].callback.apply(events[i].context || this, args);
           }
         }
